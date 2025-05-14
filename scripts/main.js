c844c7c7c2dc6d66eb08f8eee5d9084894669181
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2025-05-14 10:09:28 +0200

    feat : Added basic structure and files for webpage

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const filename = 'main.js'
+const start = Date.now();
+console.info(filename + " loaded.")
+
+/* --------------------------------- */
+
+console.log(d3);
+
+/* --------------------------------- */
+
+const end = Date.now();
+console.log(`Execution time: ${end - start} ms`);
