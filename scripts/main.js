52f16f73b873096def9d7ef62295a35b96607085
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2025-06-04 10:49:43 +0200

    feat : Fix button scroll and carousel height

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,11 @@ console.info(filename + " loaded.")
 
 console.log(`D3.js version %c${d3.version}`, "color:#a980c4; font-weight:bold;");
 
+function scrollToCarousel() {
+	const carousel = document.getElementById('carousel');
+	carousel.scrollIntoView();
+}
+
 /* --------------------------------- */
 
 const end = Date.now();
