27e30b70007095fbe59a0b4dd93e14fefdb87d54
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2025-06-11 11:07:56 +0200

    feat : Add first template for visualization and CSV loading
    - Cleanup main.js
    - Enhance CSV class
    - Fix HTML

--- scripts/csv_tools.js
+++ scripts/csv_tools.js
@@ -1,52 +1,32 @@
-class CSV {
-	static filename = 'assets/checkins.csv';
-	static columns = [
-		'checkin_id',
-		'created_at',
-		'checkin_comment',
-		'rating_score',
-		'user_id',
-		'user_name',
-		'beer_id',
-		'beer_name',
-		'beer_style',
-		'beer_slug',
-		'beer_abv',
-		'beer_link',
-		'brewery_id',
-		'brewery_name',
-		'brewery_type',
-		'brewery_country',
-		'brewery_city',
-		'brewery_link',
-		'source_app',
-		'toasts_count',
-		'venue_id',
-		'venue_name',
-		'venue_country',
-		'venue_city',
-		'venue_lat',
-		'venue_lng',
-		'avg_rating',
-		'ratings_count',
-		'checkins_count'
-	];
+// scripts/csv_tools.js
+export class CSV {
+  static filename = "assets/checkins.csv"; // Corrected path from your original post
+  static columns = null;
+  static csv = null;
 
-	static async readCSV() {
-    const response = await fetch(this.filename);
-    const data = await response.text();
-    return data;
-	}
+  /* ---------- Initializer ---------- */
 
-	/**
-	 * Get the index of a column from the name
-	 * @static
-	 * @memberof CSV
-	 * @param {String} colname
-	 */
-	static getColumnIndex(colname) {
-		let i = this.columns.indexOf(colname);
-		if (isNaN(i)) console.error(`Column ${colname} doesn't exist.`);
-		return i;
-	}
+  static async readCSV() {
+    this.csv = await d3.csv(this.filename);
+    this.columns = this.csv.columns;
+  }
+
+  /* ----------- Functions ----------- */
+
+  /**
+   * Get the index of a column from the name
+   * @static
+   * @memberof CSV
+   * @param {String} colname
+   */
+  static getColumnIndex(colname) {
+    let i = this.columns.indexOf(colname);
+    if (isNaN(i)) console.error(`Column ${colname} doesn't exist.`);
+    return i;
+  }
+
+  /* --------------- Getters --------------- */
+
+  static getCSV() { return this.csv; }
+  static getColums() { return this.columns; }
 }
