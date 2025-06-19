05deefd56e72c761389c449ac9e9399e1815de8a
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2025-06-19 10:30:05 +0200

    feat: Graph done beers per breweries

--- scripts/visualizations/beers_per_brewery.js
+++ scripts/visualizations/beers_per_brewery.js
@@ -2,70 +2,123 @@ var data, cols;
 
 async function loadData() {
   await d3.csv("assets/checkins.csv").then(function (csvData) {
-    // - Group by 'brewery_name'.
-    // - For each brewery group (v), create a Set of unique 'beer_name's
-    //   and get its size. This gives you the count of unique beers.
+    /** Group by 'brewery_name'.
+     * For each brewery group (v), create a Set of unique 'beer_name's
+     * and get its size, i.e. count of unique beers.
+     */
     const breweryData = d3.rollup(
       csvData,
       (v) => new Set(v.map((d) => d.beer_name)).size,
       (d) => d.brewery_name
     );
 
-    data = Array.from(breweryData, ([brewery_name, num_beers]) => ({ brewery_name, num_beers }));
+    data = Array.from(breweryData, ([brewery_name, num_beers]) => ({
+      brewery_name,
+      num_beers,
+    }));
+    
+    // Sort by number of beers
+    data.sort((a, b) => b.num_beers - a.num_beers);
+
+    // Only keep biggest
+    data = data.slice(0, 25);
   });
 }
 
-async function main() {
-	await loadData();
-  console.log(data);
-  
-  let margin = {top: 20, right: 30, bottom: 40, left: 30};
-  let width = 600 - margin.left - margin.right;
-  let height = 900 - margin.top - margin.bottom; 
-
-  // append the svg object to the body of the page
-  var svg = d3.select("#viz-1")
-  .append("svg")
+function makeSVG(width, height, margin) {
+  let ret = d3
+    .select("#viz-1")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // X axis: scale and draw:
-  var x = d3.scaleLinear()
-      .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+  return ret;
+}
 
-  // set the parameters for the histogram
-  var histogram = d3.histogram()
-      .value(function(d) { return d.price; })   // I need to give the vector of value
-      .domain(x.domain())  // then the domain of the graphic
-      .thresholds(x.ticks(70)); // then the numbers of bins
+function makeXaxis(width, data) {
+  // X axis for brewery names
+  let x = d3
+    .scaleBand()
+    .domain(data.map((d) => d.brewery_name))
+    .range([0, width])
+    .padding(0.1);
 
-  // And apply this function to data to get the bins
-  var bins = histogram(data);
+  return x;
+}
 
-  // Y axis: scale and draw:
-  var y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-  svg.append("g")
-      .call(d3.axisLeft(y));
+function makeYaxis(height, data) {
+  // Y axis for number of beers
+  let y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.num_beers)])
+    .range([height, 0]);
 
-  // append the bar rectangles to the svg element
-  svg.selectAll("rect")
-      .data(bins)
-      .enter()
-      .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-        .attr("height", function(d) { return height - y(d.length); })
-        .style("fill", "#69b3a2")
+  return y;
+}
+
+async function main() {
+  await loadData();
+  console.log(data);
+
+  const margin = { top: 20, right: 30, bottom: 180, left: 60 }; // Increased bottom margin for brewery names
+  const width = 900 - margin.left - margin.right; // Increased width
+  const height = 800 - margin.top - margin.bottom; // Adjusted height
+
+  // Create SVG
+  var svg = makeSVG(width, height, margin);
+
+  // X axis (brewery names)
+  var x = makeXaxis(width, data);
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-45)"); // Rotate labels for readability
+
+  // Y axis (number of beers)
+  var y = makeYaxis(height, data);
+  svg.append("g").call(d3.axisLeft(y));
+
+  // Add Y axis label
+  svg
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - height / 2)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Number of Different Beers");
+
+  // Create bars
+  svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d) => x(d.brewery_name))
+    .attr("width", x.bandwidth())
+    .attr("y", (d) => y(d.num_beers))
+    .attr("height", (d) => height - y(d.num_beers))
+    .style("fill", "#69b3a2");
+
+  // Add value labels on top of bars
+  svg
+    .selectAll(".label")
+    .data(data)
+    .enter()
+    .append("text")
+    .attr("class", "label")
+    .attr("x", (d) => x(d.brewery_name) + x.bandwidth() / 2)
+    .attr("y", (d) => y(d.num_beers) - 5)
+    .attr("text-anchor", "middle")
+    .style("font-size", "12px")
+    .text((d) => d.num_beers);
 }
 
 main();
